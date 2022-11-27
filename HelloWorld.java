d3b4b3b8d1006108920884972229551ffe9c8807
somesh.1903 <imsomeshkd@gmail.com>
2022-11-27 22:07:48 +0530

    commit 10.07 pm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,16 @@
+import java.util.Locale;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("hello");
+
+        String str = somesh("str");
+
     }
+
+    private static String somesh(String str) {
+        return str.toLowerCase(Locale.ROOT);
+    }
+
+
 }
